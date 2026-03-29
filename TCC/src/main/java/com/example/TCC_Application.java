3b7cfe88d1307ed97458cff13a71f35304e2f02c
Eddie Alencar Costa <eddiealencarcosta@gmail.com>
2026-03-29 07:18:15 -0300

    feature: Fim da criação inicial das pastas do projeto

---
+++ TCC/src/main/java/com/example/TCC_Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TCC_Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TCC_Application.class, args);
+	}
+
+}
