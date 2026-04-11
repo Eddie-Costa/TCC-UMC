d0aadf28e1c9d9b0d52b2ea3c3f7a86f896ccd9a
Matheus Santhiago <matheus_santhiago2012@hotmail.com>
2026-04-11 20:17:51 -0300

    Validao dupla
    Criacao da validao dupla

--- TCC/src/main/java/com/example/controller/LoginPageController.java
+++ TCC/src/main/java/com/example/controller/LoginPageController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import com.example.dto.UsuarioDTO;
+import jakarta.servlet.http.HttpSession; // Modifiquei
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,24 +24,53 @@ public class LoginPageController {
     public String fazerLogin(
             @Valid @ModelAttribute("usuario") UsuarioDTO usuario,
             BindingResult result,
-            Model model) throws SQLException {
+            Model model,
+            HttpSession session) throws SQLException { // Modifiquei
 
         if (result.hasErrors()) {
             return "loginPage";
         }
 
-//        //troca por query
-//        if ("admin@email.com".equals(usuario.getEmail()) &&
-//                "123456".equals(usuario.getSenha())) {
-//
-//            return "telaSucesso";
-//        }
         usuarioDAO usuarioDAO = new usuarioDAO();
-        if(usuarioDAO.QueryLoginUsuario(usuario.getEmail(),usuario.getSenha()) == "1"){
+
+        String resultado = usuarioDAO.QueryLoginUsuario(usuario.getEmail(), usuario.getSenha());
+
+        // precisa validar 2FA
+        if ("2FA".equals(resultado)) {
+            session.setAttribute("email2FA", usuario.getEmail());
+            return "pagina2fa";
+        }
+
+        // login direto
+        if ("1".equals(resultado)) {
             return "telaSucesso";
         }
 
+        // erro
         model.addAttribute("erroLogin", "Email ou senha inválidos");
         return "loginPage";
+    }
+
+    @PostMapping("/validar2fa")
+    public String validar2FA(
+            @RequestParam String codigo,
+            HttpSession session,
+            Model model) throws SQLException {
+
+        String email = (String) session.getAttribute("email2FA");
+
+        if (email == null) {
+            return "loginPage"; 
+        }
+
+        usuarioDAO usuarioDAO = new usuarioDAO();
+
+        if ("1".equals(usuarioDAO.validar2FA(email, codigo))) {
+            session.removeAttribute("email2FA"); 
+            return "telaSucesso";
+        }
+
+        model.addAttribute("erro", "Código inválido ou expirado");
+        return "pagina2fa";
     }
 }
