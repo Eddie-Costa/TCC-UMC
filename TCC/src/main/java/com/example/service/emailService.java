59d42ce244551fb0e52f3aca441abc3f287eec9e
Eddie Alencar Costa <eddiealencarcosta@gmail.com>
2026-05-16 16:23:45 -0300

    Fim da implementação BREVO

--- TCC/src/main/java/com/example/service/emailService.java
+++ TCC/src/main/java/com/example/service/emailService.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -16,11 +17,15 @@ public class emailService {
     public void enviarCodigo(String para, String codigo) {
 
         SimpleMailMessage mensagem = new SimpleMailMessage();
+
+        mensagem.setFrom("tccumcriqedmat@gmail.com");
         mensagem.setTo(para);
         mensagem.setSubject("Código de verificação - 2FA");
         mensagem.setText("Seu código de verificação é: " + codigo);
 
         mailSender.send(mensagem);
+
+        System.out.println("Email enviado com sucesso!");
     }
 
 }
